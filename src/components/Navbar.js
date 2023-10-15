b0b4eb5463b428674d6ecd61c72882cc426c40c4
mateuszkuczykkuczynski <mati.kuczykk@gmail.com>
2023-10-15 23:46:44 +0200

    CORE: Project init v2

---
+++ src/components/Navbar.js
@@ -0,0 +1,184 @@
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+
+
+const navigation = [
+    { name: 'Products', href: '#', current: false },
+    { name: 'Home', href: '#', current: true },
+    { name: 'About Us', href: '#', current: false },
+    { name: 'Contact', href: '#', current: false },
+]
+
+const moveInArray = function (arr, from, to) {
+    let item = arr.splice(from, 1);  // extract the item from its original position
+    arr.splice(to, 0, item[0]);      // insert the item to its new position
+};
+
+
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
+
+export default function Navbar() {
+    return (
+        <Disclosure as="nav" className="bg-gray-800">
+            {({ open }) => (
+                <>
+                    <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                        <div className="relative flex h-16 items-center justify-between">
+                            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                                {/* Mobile menu button*/}
+                                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                    <span className="absolute -inset-0.5" />
+                                    <span className="sr-only">Open main menu</span>
+                                    {open ? (
+                                        <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                                    ) : (
+                                        <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                                    )}
+                                </Disclosure.Button>
+                            </div>
+                            <div className="flex flex-1 justify-center">
+                                <div className="flex flex-shrink-0 items-center">
+                                    <h1 className="font-medium text-stone-50 text-lg">Darchem</h1>
+                                </div>
+                                {/* Spacer */}
+                                <div className="sm:flex-grow"></div>
+
+                                <div className="hidden sm:ml-6 sm:block">
+                                    <div className="flex space-x-4">
+                                        {navigation.map((item) => (
+                                            item.name === "Products" ?
+                                                (<Menu as="div" className="relative inline-block text-left">
+                                                    <div>
+                                                        <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                                                            {item.name}
+                                                            <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+                                                        </Menu.Button>
+                                                    </div>
+                                                    <Transition
+                                                        as={Fragment}
+                                                        enter="transition ease-out duration-100"
+                                                        enterFrom="transform opacity-0 scale-95"
+                                                        enterTo="transform opacity-100 scale-100"
+                                                        leave="transition ease-in duration-75"
+                                                        leaveFrom="transform opacity-100 scale-100"
+                                                        leaveTo="transform opacity-0 scale-95"
+                                                    >
+                                                        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                                            <div className="py-1">
+                                                                <Menu.Item>
+                                                                    {({ active }) => (
+                                                                        <a
+                                                                            href="#"
+                                                                            className={classNames(
+                                                                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                                                'block px-4 py-2 text-sm'
+                                                                            )}
+                                                                        >
+                                                                            Product Category 1
+                                                                        </a>
+                                                                    )}
+                                                                </Menu.Item>
+                                                            </div>
+                                                            <div className="py-1">
+                                                                <Menu.Item>
+                                                                    {({ active }) => (
+                                                                        <a
+                                                                            href="#"
+                                                                            className={classNames(
+                                                                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                                                'block px-4 py-2 text-sm'
+                                                                            )}
+                                                                        >
+                                                                            Product Category 2
+                                                                        </a>
+                                                                    )}
+                                                                </Menu.Item>
+                                                            </div>
+                                                            <div className="py-1">
+                                                                <Menu.Item>
+                                                                    {({ active }) => (
+                                                                        <a
+                                                                            href="#"
+                                                                            className={classNames(
+                                                                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                                                'block px-4 py-2 text-sm'
+                                                                            )}
+                                                                        >
+                                                                            Product Category 3
+                                                                        </a>
+                                                                    )}
+                                                                </Menu.Item>
+                                                                </div>
+                                                            <div className="py-1">
+                                                                <Menu.Item>
+                                                                    {({ active }) => (
+                                                                        <a
+                                                                            href="#"
+                                                                            className={classNames(
+                                                                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                                                                'block px-4 py-2 text-sm'
+                                                                            )}
+                                                                        >
+                                                                            Product Category 4
+                                                                        </a>
+                                                                    )}
+                                                                </Menu.Item>
+                                                            </div>
+                                                        </Menu.Items>
+                                                    </Transition>
+                                                </Menu>):
+                                                (<a
+                                                        key={item.name}
+                                                        href={item.href}
+                                                        className={classNames(
+                                                            item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                                            'rounded-md px-3 py-2 text-sm font-medium'
+                                                        )}
+                                                        aria-current={item.current ? 'page' : undefined}
+                                                    >
+                                                        {item.name}
+                                                    </a>
+                                                )
+                                        ))}
+                                    </div>
+                                </div>
+                                {/* Spacer */}
+                                <div className="sm:flex-grow sm:w-24"></div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <Disclosure.Panel className="sm:hidden">
+                        <div className="space-y-1 px-2 pb-3 pt-2">
+                            {(() => {
+                                let reorderedNavigation = [...navigation];  // Creating a copy of the navigation array
+                                moveInArray(reorderedNavigation, 0, 1);
+
+                                return reorderedNavigation.map((item) => (
+                                    <Disclosure.Button
+                                        key={item.name}
+                                        as="a"
+                                        href={item.href}
+                                        className={classNames(
+                                            item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                            'block rounded-md px-3 py-2 text-base font-medium'
+                                        )}
+                                        aria-current={item.current ? 'page' : undefined}
+                                    >
+                                        {item.name}
+                                    </Disclosure.Button>
+                                ));
+                            })()}
+                        </div>
+                    </Disclosure.Panel>
+                </>
+            )}
+        </Disclosure>
+
+    )
+}
