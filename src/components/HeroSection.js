7b2c89ab2026373f4934530f3ece574d057a59a8
mateuszkuczykkuczynski <mati.kuczykk@gmail.com>
2023-10-18 23:48:19 +0200

    FEAT: hero section update

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,17 +1,13 @@
 import React from 'react';
+import exampleImage from './heroback.jpeg';
+
 
 const Hero = () => {
     return (
-        <div className="bg-fixed" style={{ backgroundImage: "url('https://images.pexels.com/photos/1639562/pexels-photo-1639562.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')" }}>
 
-            <div className="flex justify-center mx-auto p-4">
-                <button className="hover:underline bg-white text-gray-800 font-bold rounded-full py-4 px-8 mr-2">
-                    More details
-                </button>
+        <div className="bg-auto bg-no-repeat bg-center ..." style={{ backgroundImage: `url(${exampleImage})` }}></div>
 
-            </div>
-        </div>
-    );
+);
 };
 
 export default Hero;
